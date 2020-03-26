32c0ddca2bfc97d44c4df2d6daf86d581ab12e22
nssy <624029344@qq.com>
2020-03-26 20:55:30 +0800

    add tls method

--- client/client_cc.go
+++ client/client_cc.go
@@ -10,6 +10,8 @@ import (
 	"github.com/zjmnssy/zlog"
 	"google.golang.org/grpc"
 
+	"google.golang.org/grpc/credentials"
+
 	// 加载健康检查库
 	_ "google.golang.org/grpc/health"
 )
@@ -97,6 +99,62 @@ func GetGrpcConn(serviceName string, scheme string) (*grpc.ClientConn, error) {
 		grpc.WithInsecure(),
 		grpc.WithBackoffMaxDelay(time.Second),
 		grpc.WithDisableServiceConfig(),
+		grpc.WithDefaultServiceConfig(string(bytes)),
+	)
+	if err != nil {
+		zlog.Prints(zlog.Warn, "zgrpc", "grpc dial error = %s", err)
+		return nil, err
+	}
+
+	return cc, nil
+}
+
+// GetGrpcConnTLS 获取GRPC TLS client连接
+func GetGrpcConnTLS(serviceName string, scheme string, certFile string, keyFile string) (*grpc.ClientConn, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(2)*time.Second)
+	defer cancel()
+
+	var serverConfig ServerConfig
+	serverConfig.LoadBalancingPolicy = balancer.Random
+	var methodConfig = MethodConfigUnit{Name: make([]NameUnit, 0, 0)}
+	var nameUnit = NameUnit{Service: serviceName}
+	methodConfig.Name = append(methodConfig.Name, nameUnit)
+	var retryPolicy = RetryPolicy{RetryableStatusCodes: make([]int, 0, 0)}
+	retryPolicy.RetryableStatusCodes = append(retryPolicy.RetryableStatusCodes, 14)
+	retryPolicy.MaxAttempts = 3
+	retryPolicy.InitialBackoff = "0.1s"
+	retryPolicy.MaxBackoff = "1s"
+	retryPolicy.BackoffMultiplier = 1
+	methodConfig.RetryPolicy = retryPolicy
+	methodConfig.WaitForReady = true
+	methodConfig.Timeout = "1.5s"
+	methodConfig.MaxRequestMessageBytes = 1024 * 1024 * 1024
+	methodConfig.MaxResponseMessageBytes = 1024 * 1024 * 1024
+	serverConfig.MethodConfig = make([]MethodConfigUnit, 0, 0)
+	serverConfig.MethodConfig = append(serverConfig.MethodConfig, methodConfig)
+	var retryThrottling = RetryThrottling{MaxTokens: 1000, TokenRatio: 1}
+	serverConfig.RetryThrottling = retryThrottling
+	var healthCheckConfig = HealthCheckConfig{ServiceName: serviceName}
+	serverConfig.HealthCheckConfig = healthCheckConfig
+
+	bytes, err := json.Marshal(serverConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if err != nil {
+		zlog.Prints(zlog.Warn, "zgrpc", "get creds error = %s", err)
+		return nil, err
+	}
+
+	cc, err := grpc.DialContext(ctx,
+		fmt.Sprintf("%s:///", scheme),
+		//grpc.WithBlock(), // 如果使用WithBlock()， 此接口返回失败，导致外面调用不好处理， 可能进入不了服务发现和负载均衡
+		grpc.WithInsecure(),
+		grpc.WithBackoffMaxDelay(time.Second),
+		grpc.WithDisableServiceConfig(),
+		grpc.WithTransportCredentials(creds),
 		grpc.WithDefaultServiceConfig(string(bytes)),
 	)
 	if err != nil {
--- server/server.go
+++ server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/zjmnssy/serviceRD/service"
 	"github.com/zjmnssy/zlog"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // GetGrpcServer 获取GRPC server
@@ -18,6 +19,28 @@ func GetGrpcServer(c etcd.Config, desc service.Desc, serviceName string, ttl int
 	}
 
 	s := grpc.NewServer()
+
+	m := health.GetManager()
+	m.Register(s, serviceName)
+
+	return s, impl, nil
+}
+
+// GetGrpcServerTLS 获取GRPC TLS server
+func GetGrpcServerTLS(c etcd.Config, desc service.Desc, serviceName string, ttl int64, certFile string, keyFile string) (*grpc.Server, *registrar.Registrar, error) {
+	impl, err := registrar.NewRegistrar(c, desc, ttl)
+	if err != nil {
+		zlog.Prints(zlog.Warn, "zgrpc", "create new register error = %s", err)
+		return nil, nil, err
+	}
+
+	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if err != nil {
+		zlog.Prints(zlog.Warn, "zgrpc", "get creds error = %s", err)
+		return nil, nil, err
+	}
+
+	s := grpc.NewServer(grpc.Creds(creds))
 
 	m := health.GetManager()
 	m.Register(s, serviceName)
